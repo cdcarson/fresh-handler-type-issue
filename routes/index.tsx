3fe1886ae9d38f2cdaebc9e2d27cd8c90f05b956
Christopher Carson <chris@nowzoo.com>
2022-07-05 17:14:27 -0500

    rename type to make it clear that it is in userland

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,12 +1,12 @@
 /** @jsx h */
 import { h } from "preact";
-import { MiddlewareState } from '../types.ts';
+import { MyMiddlewareState } from '../types.ts';
 import { HandlerContext, Handlers, PageProps } from '$fresh/server.ts';
 
 type RouteData = { foo: string, bar: number };
 export const handler: Handlers = {
-  GET: async (request: Request, ctx: HandlerContext<RouteData, MiddlewareState>) => {
-    const resp = await ctx.render({...(ctx.state as MiddlewareState)});
+  GET: async (request: Request, ctx: HandlerContext<RouteData, MyMiddlewareState>) => {
+    const resp = await ctx.render({...(ctx.state as MyMiddlewareState)});
     resp.headers.set('X-Custom-Header', 'Hello');
     return resp;
   },
